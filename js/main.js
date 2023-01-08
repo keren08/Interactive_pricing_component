b83a3e37be6138eb4c4919ca8042b44afa821f8d
keren ramos <keren.ramoss@gmail.com>
2023-01-07 20:29:28 -0600

    estilos iniciales, range con js y funcion js para actualizar el precio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 import "../assets/css/style.scss";
+import slider from "./module/slider.js";
+
+const sliderInput = document.getElementById("slider");
+const price = document.querySelectorAll(".priceN");
+
+slider(sliderInput, price);
+
+sliderInput.addEventListener("change", (e) => {
+  slider(sliderInput, price);
+});
