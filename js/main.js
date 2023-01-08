1b7a77cde080b810928c5c4859af3056c9f5dd6f
keren ramos <keren.ramoss@gmail.com>
2023-01-08 11:34:02 -0600

    diseño movil terminado y funcion para sacar el descuento al pasar pago por año

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 import "../assets/css/style.scss";
 import slider from "./module/slider.js";
+import discount from "./module/discount.js";
 
 const sliderInput = document.getElementById("slider");
 const price = document.querySelectorAll(".priceN");
+const switchF = document.getElementById("switch");
 
 slider(sliderInput, price);
 
 sliderInput.addEventListener("change", (e) => {
   slider(sliderInput, price);
+});
+
+switchF.addEventListener("click", () => {
+  const inputSwicth = document.getElementById("inputS");
+  switchF.classList.toggle("circle__active");
+  if (inputSwicth.value == 1) {
+    inputSwicth.value = 0;
+    discount(sliderInput, price, 0);
+  } else {
+    inputSwicth.value = 1;
+    discount(sliderInput, price, 1);
+  }
 });
